6f95f1d0b3624a519a0b2fcc8fb8285a56fa8da6
revant.sinha@gmail.com <revant.sinha@gmail.com>
2026-09-20 02:19:10 +0530

    feat: add some basic datastructures

--- simulator/collections/queue.go
+++ simulator/collections/queue.go
@@ -1,1 +1,54 @@
 package collections
+
+type queueNode[T any] struct {
+	value interface{}
+	next  *queueNode[T]
+}
+type Queue[T any] struct {
+	head *queueNode[T]
+	tail *queueNode[T]
+	size int
+}
+
+func NewQueue[T any]() *Queue[T] {
+	return &Queue[T]{
+		size: 0,
+	}
+}
+func (q *Queue[T]) Enqueue(v T) {
+	node := &queueNode[T]{value: v}
+	if q.head == nil {
+		q.head = node
+		q.tail = node
+	} else {
+		q.tail.next = node
+		q.tail = node
+	}
+	q.size++
+}
+
+func (q *Queue[T]) Front() T {
+	if q.IsEmpty() {
+		panic("queue is empty")
+	}
+	return q.head.value
+}
+func (q *Queue[T]) Dequeue() {
+	if q.IsEmpty() {
+		panic("queue is empty")
+	}
+
+	q.head = q.head.next
+	q.size--
+	if q.head == nil {
+		q.tail = nil
+	}
+}
+
+func (q *Queue[T]) IsEmpty() bool {
+	return q.size == 0
+}
+
+func (q *Queue[T]) Size() int {
+	return q.size
+}
--- simulator/collections/stack.go
+++ simulator/collections/stack.go
@@ -1,1 +1,59 @@
 package collections
+
+type stackNode[T any] struct {
+	value T
+	prev  *stackNode[T]
+}
+
+type Stack[T any] struct {
+	top  *stackNode[T]
+	head *stackNode[T]
+	size int
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{size: 0}
+}
+
+func (st *Stack[T]) IsEmpty() bool {
+	return st.size == 0
+}
+
+func (st *Stack[T]) Push(val T) {
+	node := &stackNode[T]{value: val}
+	if st.IsEmpty() {
+		st.head = node
+	} else {
+		node.prev = st.top
+		st.top = node
+	}
+	st.size++
+}
+
+func (st *Stack[T]) Pop() {
+	if st.IsEmpty() {
+		panic("stack is empty")
+	}
+	st.top = st.top.prev
+	st.size--
+	if st.top == nil {
+		st.head = nil
+	}
+}
+
+func (st *Stack[T]) Size() int {
+	return st.size
+}
+
+func (st *Stack[T]) Top() T {
+	if st.IsEmpty() {
+		panic("empty stack")
+	}
+	return st.top.value
+}
+
+func (st *Stack[T]) Clear() {
+	st.head = nil
+	st.top = nil
+	st.size = 0
+}
